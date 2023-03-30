04135bfddc5415b758bc8b6de865ceb568602630
Hawa <hawa.mjh@gmail.com>
2023-03-30 21:14:53 +0000

    completed assignment

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual,expected){
+  if (actual === expected){
+    console.log("✅ Assertion Passed: ${actual} === ${expected}");  
+  } else{
+  console.log("❌ Assertion Failed: ${actual} !== ${expected}");
+}
+
+}
+
+// Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("hawa","hawa")
+assertEqual(3,5)
