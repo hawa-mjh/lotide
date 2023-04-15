65c5212d1e0681429751f36da979dd91b4744a54
Hawa <hawa.mjh@gmail.com>
2023-04-15 15:50:39 +0000

    final changes

--- head.js
+++ head.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual,expected){
-  if (actual === expected){
-    console.log("✅ Assertion Passed: ${actual} === ${expected}");  
-  } else{
-  console.log("❌ Assertion Failed: ${actual} !== ${expected}");
-}
-
-}
-
+const assertEqual = require("./assertEqual");
 function head(arr){
   return arr[0];
 }
@@ -14,5 +6,5 @@ function head(arr){
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([]),);
-
+module.exports = head;
 
