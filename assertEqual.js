8bedd4a53b908cdcb1df1bc3d3b354f92f1ce4dc
Hawa <hawa.mjh@gmail.com>
2023-04-15 15:47:48 +0000

    Modularizing

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,4 @@ const assertEqual = function(actual,expected){
 
 }
 
-// Test Code
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual("hawa","hawa")
-assertEqual(3,5)
+module.exports = assertEqual;
