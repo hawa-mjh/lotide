4e3e0f319b3d685561469c3545434ad7d3dfb9f4
Hawa <hawa.mjh@gmail.com>
2023-04-15 16:04:07 +0000

    completed

--- head.js
+++ head.js
@@ -3,8 +3,6 @@ function head(arr){
   return arr[0];
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]),);
+
 module.exports = head;
 
---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+// test/assertEqualTest.js
+
+const assertEqual = require('../assertEqual');
+// Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("hawa","hawa")
+assertEqual(3,5)
