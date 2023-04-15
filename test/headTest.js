9668ae16a01222073ef178bba1eb355282b9e5f1
Hawa <hawa.mjh@gmail.com>
2023-04-15 16:03:23 +0000

    test completed

---
+++ test/headTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require('../assertEqual');
+const head = require("./head");
+
+// Test Case 1: Check if head returns the first element of an array
+assertEqual(head([1, 2, 3]), 1);
+
+// Test Case 2: Check if head returns undefined for an empty array
+assertEqual(head([]), undefined);
+
+// Test Case 3: Check if head returns the first element of an array of length 1
+assertEqual(head(["hello"]), "hello");
